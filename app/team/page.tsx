9d56d00d479c9f78f7345e7e295052b89cc9cc90
ethan <ephang08@icloud.com>
2026-08-29 22:36:11 -0700

    Update page.tsx

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -117,7 +117,6 @@ const branches = [
         name: "Kiki Yang",
         role: "Graphics Director",
         image: "/people/kiki.jpg"
-        linkedin: "https://www.linkedin.com/in/lin-qi-yang-b12565422/"
       },
       {
         name: "Namira Islam",
